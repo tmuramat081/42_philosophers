c933f6715b51d24ba944dc850bc1f28926b2e549
tmuramat <mt15hydrangea@gmail.com>
2022-12-03 11:58:07 +0900

    FIX: tidy

---
+++ philo/srcs/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmuramat <tmuramat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 00:44:31 by tmuramat          #+#    #+#             */
+/*   Updated: 2022/12/01 01:51:32 by tmuramat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	handle_error(void)
+{
+	exit(EXIT_FAILURE);
+}
+
+
+int	main(int ac, char **av)
+{
+	t_dto	args;
+
+	if (!(ac == 5 || ac == 6))
+		handle_error();
+	args = input_arguments(av);
+	solve_philos_problem(args);
+	return (0);
+}
